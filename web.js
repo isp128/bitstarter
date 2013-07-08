511b71d2332195cda43401191f35d2b09bd924b4
Sergey Itin <sergey.itin@gmail.com>
2013-07-08 21:33:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
+var buf = fs.readFileSync('index.html');
+var s = buf.toString();
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
